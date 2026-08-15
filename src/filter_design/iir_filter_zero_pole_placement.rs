0c6370d2fd2dd5294aab55d9340dcf4191d04f62
Iman Alavi Fazel <im.alavifazel@gmail.com>
2026-08-15 15:57:01 +0330

    Remove asserts from zero pole placement method

--- src/filter_design/iir_filter_zero_pole_placement.rs
+++ src/filter_design/iir_filter_zero_pole_placement.rs
@@ -1,31 +1,24 @@
+use crate::{math::convolve::convolve, types::TransferFunction};
 use num_complex::Complex;
-use crate::{
-    math::convolve::convolve,
-    types::TransferFunction
-};
 
 pub fn zero_pole_placement_iir_filter_design(
     poles: &[(f64, f64)],
     zeros: &[(f64, f64)],
 ) -> TransferFunction {
-
-    assert!(poles.len() % 2 == 0,"Number of poles must be even!");
-    assert!(zeros.len() % 2 == 0, "Number of zeros must be even!");
-
     let mut num_as_product_of_factors: Vec<Vec<Complex<f64>>> = Vec::new();
     let mut den_as_product_of_factors: Vec<Vec<Complex<f64>>> = Vec::new();
 
     for i in 0..zeros.len() {
         num_as_product_of_factors.push(vec![
             Complex::new(1.0, 0.0),
-            Complex::new(-zeros[i].0, -zeros[i].1)
+            Complex::new(-zeros[i].0, -zeros[i].1),
         ]);
     }
 
     for i in 0..poles.len() {
         den_as_product_of_factors.push(vec![
             Complex::new(1.0, 0.0),
-            Complex::new(-poles[i].0, -poles[i].1)
+            Complex::new(-poles[i].0, -poles[i].1),
         ]);
     }
 
@@ -52,18 +45,9 @@ pub fn zero_pole_placement_iir_filter_design(
     };
 
     // Extract real coefficients. They should already be pretty close to real numbers.
-    let num: Vec<f64> =
-        num_complex.iter()
-        .map(|c| c.re)
-        .collect();
+    let num: Vec<f64> = num_complex.iter().map(|c| c.re).collect();
 
-    let den: Vec<f64> =
-        den_complex.iter()
-        .map(|c| c.re)
-        .collect();
+    let den: Vec<f64> = den_complex.iter().map(|c| c.re).collect();
 
-    TransferFunction {
-        num,
-        den,
-    }
+    TransferFunction { num, den }
 }
